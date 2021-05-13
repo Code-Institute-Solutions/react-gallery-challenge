e5e79b4520b8600f00e65bc12b03972a7823efdd
John Long <johnlonggately@gmail.com>
2021-05-13 07:45:05 +0000

    forms section

--- my-app/src/components/Content.js
+++ my-app/src/components/Content.js
@@ -8,44 +8,70 @@ class Content extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            isLoaded: false
+            isLoaded: false,
+            posts: []
         }
     }
 
     componentDidMount() {
         setTimeout(()=>{
             this.setState({
-                isLoaded: true
+                isLoaded: true,
+                posts: savedPosts
             })
         }, 2000)
     }
 
+    handleChange = (e) => {
+        let name = e.target.value.toLowerCase();
+        const filteredPosts = savedPosts.filter((post)=>{
+            return post.name.toLowerCase().includes(name);
+        })
+        
+        this.setState({
+            posts: filteredPosts
+        })
+    }
     render () {
         return (
            
         <div className={css.Content}>
             <div className={css.TitleBar}>
                 <h1>My Photos</h1>
+                <form>
+                    <label htmlFor='searchinput'>Search</label>
+                    <input 
+                    type='search' 
+                    id='searchinput' 
+                    onChange={(e)=>{this.handleChange(e)}}
+                    />
+                    <h4>Posts Found: {this.state.posts.length}</h4>
+                </form>
                 
             </div>
             <div className={css.SearchResults}>
-                {/* <PostItem posts={savedPosts} /> */}
+                {
+                    this.state.isLoaded?
+                    <PostItem posts={this.state.posts} />
+                    : <Loader />
+
+                }
                
                 
 
 
                 {
-                    this.state.isLoaded ?
-                        savedPosts.map(post => {
-                            // let {title, name, image, description} = post
-                            return <div className={css.SearchItem} key={post.title}>
-                                <p>{post.title}</p>
-                                <p>{post.name}</p>
-                                <img src={post.image} alt='image'/>
-                                <p>{post.description}</p>
-                            </div>
-                        })
-                        : <Loader />
+                    // this.state.isLoaded ?
+                    //     savedPosts.map(post => {
+                    //         // let {title, name, image, description} = post
+                    //         return <div className={css.SearchItem} key={post.title}>
+                    //             <p>{post.title}</p>
+                    //             <p>{post.name}</p>
+                    //             <img src={post.image} alt='image'/>
+                    //             <p>{post.description}</p>
+                    //         </div>
+                    //     })
+                    //     : <Loader />
                     }
                     {
                     // savedPosts.map((post)=>{
--- my-app/src/components/Loader.js
+++ my-app/src/components/Loader.js
@@ -2,9 +2,9 @@ import React from 'react';
 import css from './css/Loader.module.css';
 
 const Loader = () => {
-    return <div className={css.Loader}>
-        <p>Loading...</p>
-    </div>
+    return (
+        <div className={css.Loader}> </div> 
+    )
 }
 
 export default Loader
